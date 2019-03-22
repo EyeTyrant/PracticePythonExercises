3c9cbfdbfdd509e934bf046b6298ec8b7ed61940
EyeTyrant <1414jg@gmail.com>
2019-03-22 08:05:56 -0500

    Exercise 7

---
+++ Exercise 7/listcomp.py
@@ -0,0 +1,16 @@
+# Exercise 7 List Comprehensiions
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# b = []
+# for n in a:
+#     if n % 2 == 0:
+#         b.append(n)
+# print(b)
+
+# or
+# b = [n for n in a if n % 2 == 0]
+# print(b)
+
+# or
+print([n for n in a if n % 2 == 0])
